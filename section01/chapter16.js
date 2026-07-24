b668991f76dd049627b57ee6981f7cd0e4dd5f91
Won Seokyeon <tjrdus110@gmail.com>
2026-07-24 16:00:26 +0900

    [리액트용 자바 스크립트1] Truthy & Falsy(참 같은값,거짓같은값)

---
+++ section01/chapter16.js
@@ -0,0 +1,34 @@
+// 1. Falsy한 값
+let f1 = undefined;
+let f2 = null;
+let f3 = 0;
+let f4 = 0.0;
+let f5 = NaN;
+let f6 = "";
+let f7 = false;
+
+// 2. Truthy 한 값
+// -> 7가지 Falsy 한 값들 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+// 3. 활용사례:이름을 가져오는 방법에서 객체가 아닐 때 해결하는 방법
+function printName(person) {
+  if (!person) {
+    console.log("person의 값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+let person = { name: "홍길동" };
+printName(person);
+printName2();
+
+//단락평가
+function printName2(person) {
+  let name = person && person.name;
+  console.log(name || "person name 값 없음");
+}
