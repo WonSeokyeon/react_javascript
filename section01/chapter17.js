b5d0051fdfcf0c9d1dafeeb7696e67062644e7bd
Won Seokyeon <tjrdus110@gmail.com>
2026-07-28 11:45:36 +0900

    [리액트용 자바 스크립트1]원시타입 과 객체타입 저장방식

--- section01/chapter17.js
+++ section01/chapter17.js
@@ -32,3 +32,7 @@ date1.setHours(23);
 date1.setMinutes(59); 
 date1.setSeconds(59); 
 console.log(date1); 
+
+// 5. 시간을 여러 포맷으로 출력하기 
+//시간은 제외하고 날짜만 출력하기 
+console.log(date1.toDateString()); 
---
+++ section01/chapter20.js
@@ -0,0 +1,7 @@
+let Object1={name :  "제우스", age : 40}
+let Object2={...Object1}
+ if(Object1===Object2){
+  console.log("얕은복사")
+ }else{console.log("깊은복사")}
+
+console.log(JSON.stringify(Object1)+"문자열이야")
